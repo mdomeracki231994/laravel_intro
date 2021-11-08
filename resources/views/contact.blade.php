5e1f76c94d351c6783fd86e57b39ab2624dd97ef
Mark Domeracki <mdomeracki@unitedenertech.com>
2021-11-08 17:18:49 -0500

    initinal commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,8 @@
+@extends('app')
+
+@section('content')
+    <div class="col-lg-12">
+        <h1 class="my-4">Contact</h1>
+        <p>Contact section</p>
+    </div>
+@endsection
---
+++ resources/views/services.blade.php
@@ -0,0 +1,8 @@
+@extends('app')
+
+@section('content')
+    <div class="col-lg-12">
+        <h1 class="my-4">Services</h1>
+        <p>Services section</p>
+    </div>
+@endsection
